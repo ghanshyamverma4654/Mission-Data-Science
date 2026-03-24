4b69d206de52179c69721822486f6d9617560dd7
ghanshyamverma4654 <ghanshyamofficial212@gmail.com>
2026-03-24 23:31:34 +0530

    Day 1: learned Variables, Data types, I/O, Opera.

---
+++ Day1.py
@@ -0,0 +1,33 @@
+'''1st Program'''
+#User se 2 numbers lo aur unka sum print kro.
+
+# num1 = int(input("Enter 1st number: "))
+# num2 = int(input("Enter 2nd number: "))
+# sum = num1 + num2
+# print(f"Sum of {num1} and {num2} is {sum}")
+
+'''2nd Program'''
+#Reactangle ka Area calculate karo.
+
+# length = int(input("Enter the length of rectangle: "))
+# width = int(input("Enter the width of rectangle: "))
+# area = length * width
+# print("Area of reactangle is: ", area)
+
+'''3rd Program'''
+#Celsius to Fahrenheit convert kro.
+
+# Celcius = float(input("Enter celcius value: "))
+# Fahrenheit = ((Celcius * 1.8) + 32)
+# print("Fahrenheit: ", Fahrenheit)
+
+'''4th Program'''
+#User ka naam aur age lekar print kro.
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# print(f"Hello {name}! You are {age} now.")
+
+'''5th Program'''
+#Simple Calculator
+

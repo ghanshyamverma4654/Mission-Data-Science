cccb7f6f2bd1d9f6ec0be3010550df979256a639
Ghanshyam Verma <ghanshyamofficial212@gmail.com>
2026-04-06 20:22:31 +0530

    Day7 Tuple and Set Done 👍

---
+++ Day7.py
@@ -0,0 +1,41 @@
+'''TUPLE AND SET'''
+
+'''TUPLE
+    Ordered
+    Immutable
+    Heterogeneous
+    Duplicates Allowed
+    Syntax: 
+            t = (1,2,3,4,2,5)
+    METHODS:
+            t.count(2)--- Counts the occurences of 2
+            t.index(2)--- Find the index of first occurence of 2 
+            => len()
+            => max()
+            => min()
+            => sum()
+            => sorted() --Returns new list[]
+'''
+########################################################################
+
+'''SET
+    Unordered
+    Mutable
+    Duplicates not allowed
+    Heterogeneous
+    
+    Syntax: 
+            s = {1,2,3,4,6,3,5} ---> {1,2,3,4,5,6}
+    METHODS:
+            s.add(value)
+            s.remove(value)
+            s.discard(value)
+            ------------------------
+            s.union(new set) ===> print(s | new set)
+            s.intersection(new set) ===> print(s & new set)
+            s.difference(new set) ===> print(s - new set)
+            s.symmetric_difference(new set) ===> print(s ^ new set)
+
+'''
+
+
